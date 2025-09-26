870eed73ebb2137c79ee45b17b4c6e9721c8a0ed
huafenger <95864302+huafenger@users.noreply.github.com>
2025-09-26 17:57:18 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.huafenger.top"];
+pub const RS_PUB_KEY: &str = "dTS+5YcHnujDATV2xaqB7nlKvJjQ0I6cBPYiAB4W4qo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
